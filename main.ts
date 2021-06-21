564c3d1cd72afd3a5140e8ce4064a52576da195f
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2021-06-21 17:36:22 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,3 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(kopf)
+    basic.showNumber(randint(0, 1))
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(anzahl - kopf)
-})
-let kopf = 0
-let anzahl = 0
-anzahl = 100000
-kopf = 0
-for (let index = 0; index < anzahl; index++) {
-    if (randint(0, 1) == 0) {
-        kopf += 1
-    }
-}
-basic.showString("Fertig")
